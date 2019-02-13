fed34bff42567d5ab488fdfcef35e0fc91cdd51c
Michael Chan <mchan10@stuy.edu>
2019-02-13 10:39:40 -0500

    added toString

--- KnightBoard.java
+++ KnightBoard.java
@@ -7,5 +7,31 @@ public class KnightBoard{
         board[r][c] = 0;
       }
     }
+    public String toString(){
+      boolean great = false;
+      String s = "";
+      for (int r = 0; r < board.length; r++){
+        for (int c = 0; c < board[r].length; c++){
+          if (board[r][c] > 9){
+            great = true;
+          }
+        }
+      }
+      for (int x = 0; x < board.length; x++){
+        for (int y = 0; y < board[x].length; y++){
+          if (great && board[x][y] < 10 && board[x][y] != 0){
+            s += " " + board[x][y] + " ";
+          }
+          else if (board[x][y] == 0){
+            s += "_ ";
+          }
+          else{
+            s += board[x][y] + " "
+          }
+        }
+        s += "\n"
+      }
+      return s;
+    }
   }
 }
